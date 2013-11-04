a2ab9c23a8a1a37597034ec449b30974e3a3239a
Apprentice <apprentice@dbc-21.local>
2013-11-04 14:29:44 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeMenu::Application.config.secret_token = '27b9be2cdc8552004881b3a78c010eed82f2fbf4b47c47010d27a4b86dc6d530e5fb4a5f51d020b4dca47b6b4bb107828815c88999d8aea7420f07ed1f667327'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AwesomeMenu::Application.config.session_store :cookie_store, key: '_awesome_menu_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AwesomeMenu::Application.config.session_store :active_record_store
